10bc7c07484ce4c0f21e6a2c0f684e64f18c2857
meenag0 <139080002+meenag0@users.noreply.github.com>
2024-04-07 14:30:04 -0400

    air quality map shows

--- carbonTrackr/tabs/MapScreen.tsx
+++ carbonTrackr/tabs/MapScreen.tsx
@@ -10,25 +10,25 @@ const MapScreen = () => {
   useEffect(() => {
     const fetchHeatmapData = async () => {
       try {
-        const response = await axios.get('http://localhost:8000/fetch_heatmap');
-        setHeatmapTileUrl(response.data); // Assuming the response data is the URL of the heatmap tile
-        
+        const response = await axios.get('http://localhost:8080/fetch_heatmap', {
+          responseType: 'blob'
+        });
+        setHeatmapTileUrl(URL.createObjectURL(response.data)); // Convert Blob to object URL
+        console.log("Response data:", response.data);
       } catch (error) {
         console.error('Error fetching heatmap data:', error);
       }
     };
-
+  
     fetchHeatmapData();
   }, []);
+
   console.log("Heatmap tile URL:", heatmapTileUrl);
 
-
   return (
-    
     <View style={styles.container}>
       {heatmapTileUrl ? (
-        <Image source={{ uri: heatmapTileUrl }} style={styles.map} 
-        />
+        <Image source={{ uri: heatmapTileUrl }} style={styles.map} />
       ) : (
         <Text>Loading heatmap...</Text>
       )}
@@ -45,9 +45,9 @@ const styles = StyleSheet.create({
   map: {
     width: 300, 
     height: 300, 
-    borderRadius: 10, // Add border radius for rounded corners
-    borderWidth: 2, // Add border width
-    borderColor: '#ccc', // Add border color
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#ccc',
   },
 });
 

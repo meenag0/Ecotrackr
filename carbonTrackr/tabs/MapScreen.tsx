42a746d7d56488588400c51c772994eee490c8db
meenag0 <139080002+meenag0@users.noreply.github.com>
2024-04-07 23:57:11 -0400

    added air quality based on location, debugging displaying blob response

--- carbonTrackr/tabs/MapScreen.tsx
+++ carbonTrackr/tabs/MapScreen.tsx
@@ -5,16 +5,32 @@ import { apiKey } from '../config';
 import axios from 'axios';
 
 const MapScreen = () => {
-  const [heatmapTileUrl, setHeatmapTileUrl] = useState('');
+  const [heatmapImageUrl, setHeatmapImageUrl] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
   useEffect(() => {
+
     const fetchHeatmapData = async () => {
       try {
-        const response = await axios.get('http://localhost:8080/fetch_heatmap', {
-          responseType: 'blob'
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          console.error('Permission to access location was denied');
+          return;
+        }
+    
+        // Get the user's current location
+        let location = await Location.getCurrentPositionAsync({});
+    
+        // Log location data before sending it to the server
+        console.log("Location data:", location.coords.latitude, location.coords.longitude);
+    
+        // Send location data to server and fetch heatmap data
+        const response = await axios.post('http://localhost:8080/fetch_heatmap', {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
         });
-        setHeatmapTileUrl(URL.createObjectURL(response.data)); // Convert Blob to object URL
-        console.log("Response data:", response.data);
+        setHeatmapImageUrl(URL.createObjectURL(new Blob([response.data])));
+          
       } catch (error) {
         console.error('Error fetching heatmap data:', error);
       }
@@ -23,12 +39,12 @@ const MapScreen = () => {
     fetchHeatmapData();
   }, []);
 
-  console.log("Heatmap tile URL:", heatmapTileUrl);
+  console.log("Heatmap tile URL:", heatmapImageUrl);
 
   return (
     <View style={styles.container}>
-      {heatmapTileUrl ? (
-        <Image source={{ uri: heatmapTileUrl }} style={styles.map} />
+      {heatmapImageUrl ? (
+        <Image source={{ uri: heatmapImageUrl }} style={styles.map} />
       ) : (
         <Text>Loading heatmap...</Text>
       )}
